9a3559ef5c5e3a90d4b6866a6010f4081e64804e
Jose Beneyto <sepen@crux.nu>
2023-08-15 19:15:47 +0200

    Fix cluster deletion

--- targets/default.kind.local/target.sh
+++ targets/default.kind.local/target.sh
@@ -6,15 +6,15 @@ k8kreator-cluster-create-target() {
     local name=${K8KREATOR_TARGET}
     local config=${K8KREATOR_HOME}/src/targets/${K8KREATOR_TARGET}/cluster.yaml
     k8kreator-check-deps "kind"
-    k8kreator-msg-info "Creating cluster $1"
+    k8kreator-msg-info "Creating cluster $name"
     kind create cluster --name=$name --config=$config
 }
 
 k8kreator-cluster-delete-target() {
     local name=${K8KREATOR_TARGET}
     k8kreator-check-deps "kind"
-    k8kreator-msg-info "Deleting cluster $1"
-    kind delete clusters $1
+    k8kreator-msg-info "Deleting cluster $name"
+    kind delete clusters $name
 }
 
 # End of file
