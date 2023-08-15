c17e09ecf343cfdbaa3a835d2f5b55bc4d7eae37
Jose Beneyto <sepen@crux.nu>
2023-08-15 03:18:09 +0200

    Typo and improve cluster functions in target

--- targets/default.kind.local/target.sh
+++ targets/default.kind.local/target.sh
@@ -3,14 +3,14 @@
 export K8KREATOR_TOOLS=("kind=0.20.0" "kubectl=1.27.4" "helm=3.12.3")
 
 k8kreator-cluster-create-target() {
-    local name=$1 config=$2
+    local name=${K8KREATOR_TARGET}
     k8kreator-check-deps "kind"
     k8kreator-msg-info "Creating cluster $1"
-    kind create cluster --name=$name --config=$config
+    kind create cluster --name=$name --config=cluster.yaml
 }
 
 k8kreator-cluster-delete-target() {
-    local name=$1
+    local name=${K8KREATOR_TARGET}
     k8kreator-check-deps "kind"
     k8kreator-msg-info "Deleting cluster $1"
     kind delete clusters $1
