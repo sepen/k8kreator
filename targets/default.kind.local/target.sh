13a0e95105df5b29700b07b0605c378c49550f6d
Jose Beneyto <sepen@crux.nu>
2023-08-15 03:50:36 +0200

    Fix config file for default.kind.local

--- targets/default.kind.local/target.sh
+++ targets/default.kind.local/target.sh
@@ -4,9 +4,10 @@ export K8KREATOR_TOOLS=("kind=0.20.0" "kubectl=1.27.4" "helm=3.12.3")
 
 k8kreator-cluster-create-target() {
     local name=${K8KREATOR_TARGET}
+    local config=${K8KREATOR_HOME}/src/targets/${K8KREATOR_TARGET}/cluster.yaml
     k8kreator-check-deps "kind"
     k8kreator-msg-info "Creating cluster $1"
-    kind create cluster --name=$name --config=cluster.yaml
+    kind create cluster --name=$name --config=$config
 }
 
 k8kreator-cluster-delete-target() {
