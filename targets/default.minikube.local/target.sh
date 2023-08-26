6643f10259769878bbc2b733e81a20e3966a69a2
Jose Beneyto <sepen@crux.nu>
2023-08-27 00:38:25 +0200

    Use stable versions for k8s in minikube

--- targets/default.minikube.local/target.sh
+++ targets/default.minikube.local/target.sh
@@ -2,22 +2,20 @@
 
 export K8KREATOR_TOOLS=("minikube=1.31.2" "kubectl=1.28.1" "helm=3.12.3")
 
-echo ${K8KREATOR_TOOLS[1]##*=}
-
 k8kreator-cluster-create-target() {
     local name=${K8KREATOR_TARGET}
     local config=${K8KREATOR_HOME}/src/targets/${K8KREATOR_TARGET}/cluster.yaml
-    local kversion=${K8KREATOR_TOOLS[1]##*=}
+    local minikube_version=${K8KREATOR_TOOLS[0]##*=}
     k8kreator-check-deps "minikube"
     k8kreator-msg-info "Creating cluster $name"
-    minikube start --kubernetes-version="v${kversion}" --dns-domain="${name}" --keep-context=true
+    minikube-${minikube_version} start --kubernetes-version="v1.26.8" --dns-domain="${name}" --keep-context=true
 }
 
 k8kreator-cluster-delete-target() {
     local name=${K8KREATOR_TARGET}
     k8kreator-check-deps "minikube"
     k8kreator-msg-info "Deleting cluster $name"
-    minikube delete
+    minikube-${minikube_version} delete
 }
 
 # End of file
