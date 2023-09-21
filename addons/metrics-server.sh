63fc1633829f38b98ee1ef5448c0367b0dcd154a
Jose Beneyto <sepen@crux.nu>
2023-09-21 23:26:02 +0200

    Allways use helm upgrade instead of install to simplify options to be passed

--- addons/metrics-server.sh
+++ addons/metrics-server.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 HELM_COMMAND=$(k8kreator-get-tool-command "helm")
-HELM_COMMAND_OPTS="--atomic --cleanup-on-fail"
+HELM_COMMAND_OPTS="--install --atomic --cleanup-on-fail"
 
 k8kreator-addons-install-metrics-server() {
+  k8kreator-msg-debug "Running function k8kreator-addons-install-metrics-server $@ (${K8KREATOR_TARGET})"
   local addon_version=$1
-  ${HELM_COMMAND} install metrics-server metrics-server ${HELM_COMMAND_OPTS} \
+  ${HELM_COMMAND} upgrade metrics-server metrics-server ${HELM_COMMAND_OPTS} \
     --repo https://kubernetes-sigs.github.io/metrics-server \
     --version ${addon_version} \
     --create-namespace --namespace kube-system \
@@ -13,6 +14,7 @@ k8kreator-addons-install-metrics-server() {
 }
 
 k8kreator-addons-update-metrics-server() {
+  k8kreator-msg-debug "Running function k8kreator-addons-update-metrics-server $@ (${K8KREATOR_TARGET})"
   local addon_version=$1
   ${HELM_COMMAND} upgrade metrics-server metrics-server ${HELM_COMMAND_OPTS} \
     --repo https://kubernetes-sigs.github.io/metrics-server \
@@ -22,6 +24,7 @@ k8kreator-addons-update-metrics-server() {
 }
 
 k8kreator-addons-uninstall-metrics-server() {
+  k8kreator-msg-debug "Running function k8kreator-addons-uninstall-metrics-server $@ (${K8KREATOR_TARGET})"
   local addon_version=$1
   ${HELM_COMMAND} uninstall metrics-server metrics-server \
     --namespace kube-system
