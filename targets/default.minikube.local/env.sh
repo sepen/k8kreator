c8698984062331928fbfb2b76ae1d5f43e9d93dc
Jose Beneyto <sepen@crux.nu>
2023-09-22 17:14:34 +0200

    List addons on env.sh files for each target

--- targets/default.minikube.local/env.sh
+++ targets/default.minikube.local/env.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 export K8KREATOR_TOOLS=("minikube=1.31.2" "kubectl=1.28.1" "helm=3.12.3")
-export K8KREATOR_ADDONS=()
+export K8KREATOR_ADDONS=("metrics-server=3.11.0" "metallb=0.13.4" "ingress-nginx=4.0.17")
 
 # End of file
