e793415fff149955de8c05438f7086d6640103ca
Jose Beneyto <sepen@crux.nu>
2023-10-06 13:43:41 +0200

    Fix kubernetes-dashboard version

--- targets/all.kind.local/env.sh
+++ targets/all.kind.local/env.sh
@@ -6,7 +6,7 @@ export K8KREATOR_ADDONS=(
     "metrics-server=3.11.0"
     "metallb=0.13.4"
     "ingress-nginx=4.0.17"
-    "kubernetes-dashboard=3.0.0-alpha0"
+    "kubernetes-dashboard=7.0.0-alpha1"
     "jenkins=4.6.6"
 )
 
