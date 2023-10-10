758db201d35d57e2a4c875c65a002fb6972b2218
Jose Beneyto <sepen@crux.nu>
2023-10-10 20:23:07 +0200

    kubernetes-dashboard: Fix previous commit

--- addons/kubernetes-dashboard.sh
+++ addons/kubernetes-dashboard.sh
@@ -1,6 +1,37 @@
 #!/usr/bin/env bash
 
 HELM_COMMAND=$(k8kreator-get-tool-command "helm")
+KUBECTL_COMMAND=$(k8kreator-get-tool-command "kubectl")
+
+pre-install() {
+  k8kreator-check-deps "awk"
+  cat << __YAML__ | ${KUBECTL_COMMAND} apply -f -
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRoleBinding
+metadata:
+  name: admin-user
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: cluster-admin
+subjects:
+- kind: ServiceAccount
+  name: admin-user
+  namespace: kubernetes-dashboard
+---
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: admin-user
+  namespace: kubernetes-dashboard
+__YAML__
+}
+
+post-install() {
+  k8kreator-msg-debug "Getting a Bearer Token for ServiceAccount"
+  k8kreator-msg-info "IMPORTANT: Bearer Token that can be used to log in to Dashboard"
+  ${KUBECTL_COMMAND} -n kubernetes-dashboard create token admin-user
+}
 
 k8kreator-addons-install-kubernetes-dashboard() {
   k8kreator-msg-debug "Running function k8kreator-addons-install-kubernetes-dashboard $@ (${K8KREATOR_TARGET})"
@@ -10,12 +41,15 @@ k8kreator-addons-install-kubernetes-dashboard() {
 k8kreator-addons-update-kubernetes-dashboard() {
   k8kreator-msg-debug "Running function k8kreator-addons-update-kubernetes-dashboard $@ (${K8KREATOR_TARGET})"
   local addon_version=$1
+  pre-install
   ${HELM_COMMAND} upgrade kubernetes-dashboard kubernetes-dashboard \
     --repo https://kubernetes.github.io/dashboard/ \
     --version ${addon_version} \
     --create-namespace --namespace kubernetes-dashboard \
     --install --atomic --cleanup-on-fail \
-    -f ${K8KREATOR_SRCDIR}/addons/kubernetes-dashboard/values.yaml
+    -f ${K8KREATOR_SRCDIR}/addons/kubernetes-dashboard/values.yaml \
+    --set app.ingress.hosts={dashboard.${K8KREATOR_TARGET},dashboard,localhost}
+  post-install
 }
 
 k8kreator-addons-uninstall-kubernetes-dashboard() {
