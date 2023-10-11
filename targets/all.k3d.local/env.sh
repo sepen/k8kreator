e29a33a66cfa747783fba164b5718c5823dc900c
Jose Beneyto <sepen@crux.nu>
2023-10-12 00:00:35 +0200

    Add new targets all.k3d.local and all.minikube.local

---
+++ targets/all.k3d.local/env.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+export K8KREATOR_TOOLS=("k3d=5.6.0" "kubectl=1.28.1" "helm=3.12.3")
+
+export K8KREATOR_ADDONS=(
+    "metrics-server=3.11.0"
+    "metallb=0.13.4"
+    "ingress-nginx=4.0.17"
+    "kubernetes-dashboard=7.0.0-alpha1"
+    "kubewatch=3.3.10"
+    "jenkins=4.6.6"
+    "prometheus=25.1.0"
+)
+
+# End of file
