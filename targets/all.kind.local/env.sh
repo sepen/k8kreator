c01896fdfdb89d553bf3953578a9ef6cfbb26dbb
Jose Beneyto <sepen@crux.nu>
2023-11-22 10:01:29 +0100

    Several update (kubectl, metallb, ingress-nginx) for target all.kind.local

--- targets/all.kind.local/env.sh
+++ targets/all.kind.local/env.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-export K8KREATOR_TOOLS=("kind=0.20.0" "kubectl=1.27.4" "helm=3.12.3")
+export K8KREATOR_TOOLS=("kind=0.20.0" "kubectl=1.28.4" "helm=3.13.2")
 
 export K8KREATOR_ADDONS=(
     "metrics-server=3.11.0"
-    "metallb=0.13.4"
-    "ingress-nginx=4.0.17"
+    "metallb=0.13.12"
+    "ingress-nginx=4.8.3"
     "kubernetes-dashboard=7.0.0-alpha1"
     "kubewatch=3.3.10"
     "jenkins=4.6.6"
