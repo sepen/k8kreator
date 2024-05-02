8743bf0034a4e2af2a8699efefdfe3d702862f57
Jose Beneyto <sepen@crux.nu>
2024-05-02 18:08:09 +0200

    default.kind.local: update to kubernetes v1.30.0 and kind 0.22.0

--- targets/default.kind.local/env.sh
+++ targets/default.kind.local/env.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export K8KREATOR_TOOLS=("kind=0.20.0" "kubectl=1.28.4" "helm=3.13.2")
-export K8KREATOR_ADDONS=("metrics-server=3.11.0" "metallb=0.13.12" "ingress-nginx=4.8.3")
+export K8KREATOR_TOOLS=("kind=0.22.0" "kubectl=1.30.0" "helm=3.14.0")
+export K8KREATOR_ADDONS=("metrics-server=3.12.1" "metallb=0.14.5" "ingress-nginx=4.10.1")
 
 # End of file
