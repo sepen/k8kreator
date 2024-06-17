16bcbb2ce4e1a25caeca6d680fda3aff7447aaa4
Jose Beneyto <sepen@crux.nu>
2024-06-17 23:01:04 +0200

    k8kreator.kind.local: update control-plane and nodes to 1.30.0, kind to 0.23.0 and helm to 3.15.2

--- targets/k8kreator.kind.local/env.sh
+++ targets/k8kreator.kind.local/env.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export K8KREATOR_TOOLS=("kind=0.22.0" "kubectl=1.30.0" "helm=3.14.0")
+export K8KREATOR_TOOLS=("kind=0.23.0" "kubectl=1.30.0" "helm=3.15.2")
 
 export K8KREATOR_ADDONS=(
     "metrics-server=3.12.1"
