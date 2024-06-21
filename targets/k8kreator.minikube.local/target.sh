a4e68c89a4e7782c58f705c5f5dcf5ef011b7248
Jose Beneyto <sepen@crux.nu>
2024-06-21 18:01:02 +0200

    k8kreator.minikube.local: update control-plane and nodes to 1.30.0, minikube to 1.33.1 and helm to 3.15.2

--- targets/k8kreator.minikube.local/target.sh
+++ targets/k8kreator.minikube.local/target.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
 MINIKUBE_COMMAND=$(k8kreator-get-tool-command "minikube")
-KUBERNETES_VERSION="v1.26.8"
+KUBERNETES_VERSION="v1.30.0"
 
 k8kreator-cluster-create-target() {
   k8kreator-check-deps ${MINIKUBE_COMMAND}
   ${MINIKUBE_COMMAND} start \
     --kubernetes-version="${KUBERNETES_VERSION}" \
     --dns-domain="${K8KREATOR_TARGET}" \
+    --extra-config="kubelet.cluster-domain=${K8KREATOR_TARGET}" \
     --keep-context=false
 }
 
