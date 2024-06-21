07b25f4346608177320b18aaa299b5537f84ef2a
Jose Beneyto <sepen@crux.nu>
2024-06-21 19:01:57 +0200

    k8kreator.k3d.local: update control-plane and nodes to 1.30.2, k3d to 5.6.3 and helm to 3.15.2

--- targets/k8kreator.k3d.local/env.sh
+++ targets/k8kreator.k3d.local/env.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-export K8KREATOR_TOOLS=("k3d=5.6.0" "kubectl=1.28.1" "helm=3.12.3")
+export K8KREATOR_TOOLS=("k3d=5.6.3" "kubectl=1.30.0" "helm=3.15.2")
 
 export K8KREATOR_ADDONS=(
-    "metrics-server=3.11.0"
-    "metallb=0.13.4"
-    "ingress-nginx=4.0.17"
-    "kubernetes-dashboard=7.0.0-alpha1"
+    "metrics-server=3.12.1"
+    "metallb=0.14.5"
+    "ingress-nginx=4.10.1"
+    "kubernetes-dashboard=7.3.2"
     "kubewatch=3.3.10"
-    "jenkins=4.6.6"
-    "prometheus=25.1.0"
-    "grafana=6.60.6"
-    "promtail=6.15.2"
-    "loki=5.31.0"
+    "jenkins=5.2.2"
+    "prometheus=25.20.1"
+    "grafana=7.3.9"
+    "promtail=6.15.5"
+    "loki=6.4.2"
 )
 
 # End of file
